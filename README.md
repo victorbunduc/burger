813f88ae6c3bc0a0c0b63a56b49592e2cc3adea3
victorbunduc <ciburekhouse@gmail.com>
2021-01-10 23:22:20 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# ![icon](public/assets/img/icons-hamburger.png) Eat-Da-Burger! ![icon](public/assets/img/icons-hamburger.png)
+
+Eat-Da-Burger! is a restaurant app that lets users input the names of burgers they'd like to eat. When the user submits a burger's name, the app displays the burger on the left side of the page -- waiting to be devoured. Each burger in the **Available Burgers** waiting area has a `DEVOUR IT` button. When the user clicks it, the burger moves to the right side of the page, under **Burgers Devoured**.
+
+![burger-app](public/assets/img/burger-img.png)
+
+## Demo
+
+Eat-Da-Burger is hosted on Heroku. Check it out by [clicking here].
+
+## Getting Started
+
+To use Eat-Da-Burger locally, use http://localhost:8080 after completing the following steps:
+
+
+
+## Technologies Used
+
+* MySQL
+* Node.js
+* Express
+* Handlebars
---
+++ server.js
@@ -0,0 +1,38 @@
+// Create express connection and run node server
+var express = require("express");
+var exphbs = require("express-handlebars");
+var methodOverride = require("method-override");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+// This is a level of abstraction to hide credentials from user
+app.use(express.static('public'));
+
+// Parse application body as JSON
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(express.json());
+
+// the main page is always displayed
+app.use(methodOverride('_method'));
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+// Norm is to call it "var = routes"
+var routes = require("./controllers/burgers_controller.js");
+
+// Use express routes defined
+app.use("/", routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
